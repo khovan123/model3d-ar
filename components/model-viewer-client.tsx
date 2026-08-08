b3dbe06f33892ae24f1f0d4c5bac425fd670cf23
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-08 14:36:17 +0700

    feat: mount ModelSpace runtime debugger

--- components/model-viewer-client.tsx
+++ components/model-viewer-client.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { ModelViewer as InteractionModelViewer } from "./model-viewer-interaction";
+import { ModelSpaceDebugPanel } from "./modelspace-debug-panel";
 import {
   installQuickLookTapBehaviorPatch,
   setQuickLookAudioSource
 } from "./quicklook-behavior";
 import { installQuickLookUsdFinalizePatch } from "./quicklook-usdz-finalize";
+import { installModelSpaceDebugging } from "@/lib/modelspace-debug";
 
 installQuickLookTapBehaviorPatch();
 installQuickLookUsdFinalizePatch();
+installModelSpaceDebugging();
 
 type Props = {
   modelName: string;
@@ -19,5 +22,10 @@ type Props = {
 
 export function ModelViewer(props: Props) {
   setQuickLookAudioSource(props.audioUrl);
-  return <InteractionModelViewer {...props} />;
+  return (
+    <>
+      <InteractionModelViewer {...props} />
+      <ModelSpaceDebugPanel />
+    </>
+  );
 }
